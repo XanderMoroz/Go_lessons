8f75d03f62a5fd2788dd0e219a05abdfb5ede3f1
Александр <7176uiop3436>
2023-05-03 22:02:04 +0300

    Мапы в GO (Теория и практика)

--- lesson05.go
+++ lesson05.go
@@ -19,10 +19,13 @@ func main() {
 	addToMap()
 	deleteFromMap()
 	mapIteration()
+
+	homeWork09()
 }
 
 func mapDefinition() {
 	fmt.Println("ТЕОРИЯ: СОЗДАЕМ Map'ы")
+
 	// Инициируем пустую map'у по схеме map[тип данных ключей]тип данных значений
 	var book map[string]int // не делайте так В нее нельзя ничего добавить
 
@@ -83,7 +86,7 @@ func addToMap() {
 }
 
 func deleteFromMap() {
-	fmt.Println("ТЕОРИЯ: ИЗВЛЕКАЕМ ЗНАЧЕНИЕ ИЗ МАПЫ ПО КЛЮЧУ")
+	fmt.Println("ТЕОРИЯ: УДАЛЯЕМ ЗНАЧЕНИЕ ИЗ МАПЫ ПО КЛЮЧУ")
 	// Инициируем map'у и присваиваем ей ключи и значения
 	users := map[string]bool{
 		"user01": true,
@@ -120,12 +123,48 @@ func mapIteration() {
 
 }
 
-//func homeWork09() {
-//	// Задание
-//	// 1) Создайте мап в которой необходимо хранить информацию
-//	// о выданных читателю печатных изданиях: книгах, переодических изданиях
-//	// 2) Тип ключей мапа - строка, тип значений - мапа с
-//	// [ ключем типа - строка, значением слайс строк ]
-//	// 3) Добавьте произвольные значения
-//
-//}
+func homeWork09() {
+	// Задание
+	// 1) Создайте мап в которой необходимо хранить информацию
+	// о выданных читателю печатных изданиях: книгах, переодических изданиях
+	// 2) Тип ключей мапа - строка, тип значений - мапа с
+	// [ ключем типа - строка, значением слайс строк ]
+	// 3) Добавьте произвольные значения, моделирующие нахождение у читателя книг и периодики
+	// Выведите на экран количество читателей с изданиями на руках.
+	// Выведите на экран общее количество изданий на руках одного читателя.
+
+	readersBalance := map[string]map[string]int{
+		"reader01": {
+			"book":     12,
+			"periodic": 6,
+		},
+		"reader02": {
+			"book":     8,
+			"periodic": 2,
+		},
+		"reader03": {
+			"book":     3,
+			"periodic": 7,
+		},
+	}
+	// Инициируем слайс со строками длиной равной количеству элементов в мапе
+	readers := make([]string, 0, len(readersBalance))
+
+	for reader := range readersBalance {
+		readers = append(readers, reader)
+	}
+	fmt.Println(readers, len(readers))
+
+	// Сортируем слайс ключами из мапа
+	sort.Strings(readers)
+	// В цикле обращаемся к каждому значению мапа по ключу
+	var bookCounter, periodicCounter int
+	for _, v := range readers {
+		thisReader := readersBalance[v]
+		bookCounter += thisReader["book"]
+		periodicCounter += thisReader["periodic"]
+	}
+	fmt.Println("Читателей всего:", len(readers), readers)
+	fmt.Println("На руках читателей всего:", bookCounter+periodicCounter)
+	fmt.Println("Из них книг:", bookCounter, "переодических изданий:", periodicCounter)
+}
